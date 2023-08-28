f491154d611c47bcaca1d7e2a88ce1950d2133cf
pai-natal <ivanfclemente@gmail.com>
2023-08-28 19:07:06 +0100

    first commit

---
+++ src/components/CityList.jsx
@@ -0,0 +1,25 @@
+import CityItem from "./CityItem";
+import styles from "./CityList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+import { useCities } from "../context/CitiesContext";
+
+function CityList() {
+  const { cities, isLoading } = useCities();
+
+  if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city, index) => (
+        <CityItem city={city} key={city.id} index={index} />
+      ))}
+    </ul>
+  );
+}
+
+export default CityList;
